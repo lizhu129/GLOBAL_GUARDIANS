27c6a0873b185f190d3be7c45757cdfbdfd63f3a
thomas murat <thomas.murat974@gmail.com>
2023-08-23 20:12:02 +0200

    add service model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_175844) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_181048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "services", force: :cascade do |t|
+    t.boolean "active"
+    t.string "category"
+    t.string "name"
+    t.text "description"
+    t.integer "min_service_day"
+    t.integer "price_per_day"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
