a201bbc6c1044abb9b5407ee35c5b18be8f57c88
thomas murat <thomas.murat974@gmail.com>
2023-08-23 21:23:47 +0200

    association and validation

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,11 @@
 class Service < ApplicationRecord
+  has_many :bookings
+  has_many :users
+  has_many :reviews, through: :bookings, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 10 }
+  validates :description, presence: true
+  validates :price_per_day, presence: true
+  validates :category, presence: true, inclusion: { in: ["Host family", "Babysitter", "Playmate", "Other", "Advise", "Emotionnal support", "Day outing", "Night outing", "Weekend outing"] }
+  validates :active, presence: true, inclusion: { in: [true, false] }, default: true
 end
