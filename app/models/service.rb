da5a65c093e6f1b54c95f407d2b95745681324ec
Li Zhu <lizhu129@gmail.com>
2023-08-26 20:22:02 +0200

    skeleton home page

--- app/models/service.rb
+++ app/models/service.rb
@@ -6,7 +6,7 @@ class Service < ApplicationRecord
   validates :name, presence: true, length: { minimum: 10 }
   validates :description, presence: true
   validates :price_per_day, presence: true
-  validates :category, presence: true, inclusion: { in: ["Host family", "Babysitter", "Playmate", "Other", "Advise", "Emotionnal support", "Day outing", "Night outing", "Weekend outing"] }
+  validates :category, presence: true, inclusion: { in: ["Host family", "Babysitter", "Playmate", "Other", "Advise", "Emotional support", "Day outing", "Night outing", "Weekend outing"] }
   # WIP temporary removed the default: settings in the validation as it was creating an issue
   validates :active, presence: true, inclusion: { in: [true, false] }#, default: true
 end
