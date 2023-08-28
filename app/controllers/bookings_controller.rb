95ec4a41963e1bcd4031c10f1747ac42eccc157a
thomas murat <thomas.murat974@gmail.com>
2023-08-28 22:02:43 +0200

    booking new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @service = Service.find(params[:service_id])
+    @booking = Booking.new(booking_params)
+    @booking.service = @service
+    @booking.user = current_user
+    @booking.status = "pending"
+
+    days = (@booking.end_date - @booking.start_date).to_i + 1
+    @booking.total_price = days * @service.price_per_day
+
+    if @booking.save
+      redirect_to service_path(@service)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   resources :services
+  resources :bookings, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
